f309a164308f2af5f7b81eae56436334db1f5977
llcpkull@gmail.com <llcpkull@gmail.com>
2018-12-23 13:28:03 +0100

    generic menu from configuration

--- sonoff_switch_control.py
+++ sonoff_switch_control.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 '''
 '
 ' This script only works for devices which are configured as describe here:
@@ -15,13 +15,11 @@ import requests
 
 import xml.etree.ElementTree as ET
 
-#TODO: * add requirements.txt file
-#      * import python logger
 
 def get_current_ip():
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	s.connect(("8.8.8.8", 80))
-	print(s.getsockname()[0])
+	print('\t' + s.getsockname()[0])
 	s.close()
 
 def sonoff_switch(ip_add, value):
@@ -51,79 +49,42 @@ def main():
 	tree = ET.parse('config.xml')
 	root = tree.getroot()
 
-	#TODO: Give me the # of childs
 	number_of_children = len(root.getchildren())
-	number_of_children_element = 2
-	for child in root:
-            print(child.tag)
-
-	print(root[0][0].text)
-
-
-	print("\n\rSelect from menu:\n\r-------------------")
-	for x in range(0, number_of_children):
-		print(str(x+1) + ")" + root[x][0].text)
-	print("q)Quit")
 	
-	var = input(">>")
-	if var=='q':
-		print("\tQuit App\n\r--------------------------------------------------------------")
-		sys.exit()
-
-	#TODO: True and False
-	var = int(var)
-	if var > number_of_children:
-		print("\tUndefined Selected. Retype")
-
-	print(root[var-1][0].text + " selected")
-	if sonoff_switch(root[var-1][1].text, 1):
-		print("\tSucessfully")
-	else:
-		print("\tFailed")
-
-
-	while 1:
-		print("\n\rSelect from menu:\n\r-------------------\n\r1)LED-strip-ON\n\r2)LED-strip-OFF\n\r3)OurLamp-ON\n\r4)OurLamp-OFF\n\rq)quit")
+	while(1):
+		print("\n\rSelect from menu:\n\r-------------------")
+		for x in range(0, number_of_children):
+			print(str(2*x) + ")" + root[x][0].text + " ON")
+			print(str(2*x+1) + ")" + root[x][0].text + " OFF")
+		print("q)Quit")
 
 		var = input(">>")
-                #TODO: generic made a list from .xml files
-		if var=='1':
-			print("\tOurLamp-ON")
-			if sonoff_switch("192.168.0.14", 1):
-				print("\tSucessfully")
-				continue
-			else:
-				print("\tFailed")
-				continue
-		elif var=='2':
-			print("\tOurLamp-OFF")
-			if sonoff_switch("192.168.0.14", 0):
-				print("\tSucessfully")
-				continue
-			else:
-				print("\tFailed")
-				continue
-		if var=='3':
-			print("\tLED-strip-ON")
-			if sonoff_switch("192.168.0.15", 1):
-				print("\tSucessfully")
-				continue
-			else:
-				print("\tFailed")
-				continue
-		elif var=='4':
-			print("\tLED-strip-OFF")
-			if sonoff_switch("192.168.0.15", 0):
-				print("\tSucessfully")
-				continue
-			else:
-				print("\tFailed")
-				continue
-		elif var=='q':
+		if var=='q':
 			print("\tQuit App\n\r--------------------------------------------------------------")
 			sys.exit()
-		else:
+
+		#TODO: True and False
+		var = int(var)
+		if var > 2*number_of_children-1:
 			print("\tUndefined Selected. Retype")
+			continue
+
+		#TODO: parni su ON, neparni su OFF
+		if var % 2 == 0:
+		    new_var = var/2
+		    value = 1
+		else:
+		    value = 0
+		    new_var = round( (var-1)/2, 0 )
+		new_var = int(new_var)
+
+		print(root[new_var][0].text + " selected")
+		if sonoff_switch( root[new_var][1].text, value ):
+			print("\tSucessfully")
+			continue
+		else:
+			print("\tFailed")
+			continue
 
 
 if __name__ == "__main__":
